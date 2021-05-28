34b3f3ca1ce4d1c93a7442b3679534cb0ae5be26
Richo Ha <richard.dqh@gmail.com>
2021-05-28 23:44:36 +0800

    Edited File Structure and Code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,58 +11,58 @@ var passwordCharacters = "";
 //This variable is the Password Generate Button.
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
 
   //Function to determine length.
   var passwordLength = prompt("Please specify number of charachters in Password (8-128).");
-  if(passwordLength >= 8 && passwordLength <=128){
+  if (passwordLength >= 8 && passwordLength <= 128) {
     var passwordLength = parseInt(passwordLength);
-  }else{
+  } else {
     alert("Length is underfined, try again")
-    generatePassword(); 
+    generatePassword();
   }
 
   //Give user the option to include capital letters.
   var incPasswordCapital = confirm("Do you want capital letters in your password?");
 
-  if(incPasswordCapital = true){
+  if (incPasswordCapital = true) {
     passwordCharacters += passwordCapital;
   }
 
   //Give user the option to include lower case letters.
   var incPasswordLower = confirm("Do you want lower case letters in your password?");
 
-  if(incPasswordLower = true){
+  if (incPasswordLower = true) {
     passwordCharacters += passwordLower;
   }
 
   //Give user the option to include numbers.
   var incPasswordNumber = confirm("Do you want numbers in your password?");
 
-  if(incPasswordNumber = true){
+  if (incPasswordNumber = true) {
     passwordCharacters += passwordNumber;
   }
 
   //Give user the option to include special characters.
   var incPasswordSpecial = confirm("Do you want special characters in your password?");
 
-  if(incPasswordSpecial = true){
+  if (incPasswordSpecial = true) {
     passwordCharacters += passwordSpecial;
   }
   randomisePassword()
 
-  function randomisePassword(){
+  function randomisePassword() {
     //ensure there are nothing in password
     password = "";
     //add loop to get password chars.
-    for(var i = 0; i < passwordLength; i++){
-      password += passwordCharacters.charAt(Math.floor(Math.random()*passwordCharacters.length));
+    for (var i = 0; i < passwordLength; i++) {
+      password += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
       //if password has atleast one of each specified cases than contiue.., else revoke randomisePassword.
     }
-    if(password.indexOf(passwordCapital) && password.indexOf(passwordLower) && password.indexOf(passwordNumber) && password.indexOf(passwordSpecial)){
+    if (password.indexOf(passwordCapital) && password.indexOf(passwordLower) && password.indexOf(passwordNumber) && password.indexOf(passwordSpecial)) {
       var passwordText = document.getElementById("password");
       passwordText.textContent = password;
-    }else{
+    } else {
       randomisePassword();
     }
   }
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # passWordGenerator
 A password generator with set conditions by the user.
 
-password generator github repository 
+##  password generator github repository 
 https://github.com/RichoHa/passWordGenerator
 
-password generator website link:
+##  password generator website link:
 https://richoha.github.io/passWordGenerator/
 
-![passwordGeneratorSnippet](https://user-images.githubusercontent.com/76611103/111954242-c1b5d400-8b22-11eb-96ca-78871eb8cf94.PNG)
+##  What this application does:
+This code will generate a password provided prompts from the user.
+
+##  Snippet of Code
+![passwordGeneratorSnippet](./Assets/images/passwordGeneratorSnippet.PNG)
+
